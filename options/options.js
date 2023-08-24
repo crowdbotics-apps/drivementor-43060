fe0e50ff3f66831b0215603dcce692be1e6adaaa
Crowdbotics <team@crowdbotics.com>
2023-08-24 22:42:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "drivementor-43060",
+  url: "https://drivementor-43060.botics.co",
+  api: "https://drivementor-43060.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
